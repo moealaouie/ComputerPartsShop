7eff13909c601d31dcba0ca32490480383de5dfb
MoeAlaouie <54420786+moealaouie@users.noreply.github.com>
2021-08-04 23:22:36 -0400

    commit website
    commit website

---
+++ category.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * category.php
+*
+ * Project 
+ *
+ * @category   Project
+ * @package    Project 
+ * @author     Mohamed Alaouie <malaouie@emich.edu
+ * @version    8/2/2021
+ * @link       
+ */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ template_footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*
+ * Project 
+ *
+ * @category   Project
+ * @package    Project 
+ * @author     Mohamed Alaouie <malaouie@emich.edu
+ * @version    8/2/2021
+ * @link       
+ */
+ ?>
+<div id="pageFooter">DYNAMOE CORPORATIONS Copyright ||</div>
